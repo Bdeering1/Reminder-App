cbc95d138dd60904103a944a71c2cc99b2b8f721
Bryn Deering <bdeering@uoguelph.ca>
2023-03-31 19:46:53 -0400

    Wired up post requests to pushed api

--- lib/reminder_app/notifications.ex
+++ lib/reminder_app/notifications.ex
@@ -3,28 +3,41 @@ defmodule ReminderApp.Notifications do
   use Timex
 
   def try_notify do
-    IO.puts "Checking task list"
+    IO.puts "Checking task list..."
     date = DateTime.utc_now
-    # date_edt = Timex.Timezone.convert(date, "America/Toronto")
     tasks = Tasks.list_tasks()
     for task <- tasks do
-      if task.next_date <= date do
-        if send_notification(task) == :ok do
-          IO.puts "Sending notification for \"#{task.name}\""
-          if Map.fetch(task, :interval) === {:ok, nil} do
-            Tasks.delete_task(task)
-          else
-            date = Timex.shift(task.next_date, days: task.interval)
-            Tasks.update_task(task, %{next_date: date})
-          end
-        end
-      end
+      if Timex.compare(task.next_date, date) < 1, do: send_notification(task) |> handle_response(task)
     end
   end
 
-  def send_notification(_task) do
+  def send_notification(task) do
     HTTPoison.start
-    :ok
+    url = Application.fetch_env!(:reminder_app, :url)
+    app_key = Application.fetch_env!(:reminder_app, :app_key)
+    app_secret = Application.fetch_env!(:reminder_app, :app_secret)
+    body = Poison.encode!(%{
+        app_key: app_key,
+        app_secret: app_secret,
+        target_type: "app",
+        content: task.name
+      })
+    headers = ["Content-Type": "application/json"]
+    HTTPoison.post(url <> "push", body, headers)
+  end
+
+  def handle_response(response, task) do
+    {status, res} = response
+    if status === :ok and res.status_code == 200 do
+      if Map.fetch(task, :interval) === {:ok, nil} do
+        Tasks.delete_task(task)
+      else
+        date = Timex.shift(task.next_date, days: task.interval)
+        Tasks.update_task(task, %{next_date: date})
+      end
+    end
+    %{"response" => %{"message" => message}} = Poison.decode!(res.body)
+    IO.puts "\n\"#{task.name}\" - #{message}\n"
   end
 
 end
